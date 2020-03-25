a87e8549624dea366e810e698c01c1a569f12a64
maxjwebber <tua83483@temple.edu>
2020-03-25 00:08:29 -0400

    added items to page population after getting JSON object

--- src/main/java/edu/temple/trackingserver/analytics.js
+++ src/main/java/edu/temple/trackingserver/analytics.js
@@ -2,51 +2,48 @@ let report;
 let startDate;
 let endDate;
 let Chart = require('chart.js');
+let ctx = document.getElementById('canvas').getContext('2d');
 
-function setDatesAndTimes()
+async function setDatesAndTimes()
 {
-  startDate = document.getElementById('startDate').value.toString();
-  endDate = document.getElementById('endDate').value.toString();
-  requestAndChart();
+    startDate = document.getElementById('startDate').value;
+    endDate = document.getElementById('endDate').value;
+    requestAndChart();
 }
-
-function showOrHide(div) {
-  if (div.style.display === "none") {
-    div.style.display = "block";
-  } else {
-    div.style.display = "none";
-  }
-}
-
 const requestAndChart = () => {
   fetch('/views?startDate='+startDate+'&endDate='+endDate)
       .then((response) => {
         if (!(response.statusText==="OK")) {
           throw Error(response.statusText);
         }
-        return response.json();
+        return response();
       })
-      .then((json) => {
-        report = json;
-    })
-    .finally(async () => {
-      /** @namespace report.numRequestsPerMin[] **/
-      /** @namespace report.numUniques **/
-      /** @namespace report.mostViewedURLs[] **/
-      /** @namespace report.mostViewedURLs[] **/
-      let labels = [];
-      let minutes;
-      let startDateObject = new Date(startDate);
-      let labelsSize = report.numRequestsPerMin.length;
-      for (let i = 0; i<labelsSize;i++)
-      {
-        labels[i]=new Date(startDateObject.getTime() + i*60000);
-      }
-      let ctx = document.getElementById('canvas').getContext('2d');
-      new Chart(ctx,{"type":"line","data":{"labels":labels,"datasets":[{"label":"Users Per Minute","data":report.numRequestsPerMin,"fill":false,"borderColor":"rgb(75, 192, 192)","lineTension":0.1}]},"options":{}});
+      .then((response) => {
+        report = response;
+      })
+      .finally( () => {
+        /** @namespace report.numRequestsPerMin[] **/
+        /** @namespace report.numUniques **/
+        /** @namespace report.mostViewedURLs[] **/
+        /** @namespace report.mostUsedBrowsers[] **/
+        let labels = [];
 
-  }).catch((error) => {
-    console.log(error);
+        let startDateObject = new Date(startDate);
+        let labelsSize = report.numRequestsPerMin.length;
+        for (let i = 0; i<labelsSize;i++)
+        {
+          labels[i]=new Date(startDateObject.getTime() + i*60000);
+        }
+
+        let fancyNewChart = new Chart(ctx,{"type":"line","data":{"labels":labels,"datasets":[{"label":"Users Per Minute","data":report.numRequestsPerMin,"fill":false,"borderColor":"rgb(75, 192, 192)","lineTension":0.1}]},"options":{}});
+        document.getElementById("uniques").innerHTML="There are "+report.numUniques+" unique User IDs for this period.";
+        document.getElementById("topBrowsers").innerHTML="The top browsers are:\n1) "+report.mostUsedBrowsers[0]+"\n2) "+report.mostUsedBrowsers[1]+"\n3) "+report.mostUsedBrowsers[2];
+        document.getElementById("topSites").innerHTML="The top sites are:\n1) "+report.mostViewedURLs[0]+"\n2) "+report.mostViewedURLs[1]+"\n3) "+report.mostViewedURLs[2];
+      }).catch((error) => {
+      console.log(error);
 
   })
-  };
+};
+
+
+
